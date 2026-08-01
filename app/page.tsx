3fa1206ad94af5dd95a5bec4d4d37f5a97c0d90b
Rayusha <mrayusha8@gmail.com>
2026-08-01 17:02:33 +0545

    Correct the name of footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/layout/Footer";
+import { Footer } from "@/components/layout/footer";
 import { Navbar } from "@/components/layout/Navbar";
 import { InsightPreview } from "@/components/ui/InsightPreview";
 import { SectionHeading } from "@/components/ui/SectionHeading";
